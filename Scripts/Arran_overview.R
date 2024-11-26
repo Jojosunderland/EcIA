6ffef91ce8a41aac09d7cb27ecefa8dab2dfa989
Jojosunderland <jojolucysunderland@gmail.com>
2024-11-26 23:46:52 +0000

    site by species presence absence matrix created

--- Scripts/Arran_overview.R
+++ Scripts/Arran_overview.R
@@ -29,4 +29,26 @@ arran.dat.south <-arran.dat[which(arran.dat$site=="South"),]
 length(unique(arran.dat.north$order)) # 25 unique orders
 length(unique(arran.dat.south$order)) # 25 unique orders
 
+## Create a site by species matrix ##
 
+# ONLY presence/absence data - as verts dont have counts
+arran.dat.new <- arran.dat[,c("order","site")] 
+arran.dat.new$presence <- 1  # adds a new column presences (sets everything to 1s)
+
+# group by site, order and sum the values (presence data) for each group 
+arran.dat.new <- arran.dat.new %>%
+  group_by(site, order) %>%
+  summarise(presence = sum(presence), .groups = "drop")
+
+arran.dat.pa <- arran.dat.new %>% 
+  pivot_wider(names_from=order,values_from=c(presence)) 
+# pivot_wider creates a wide-format data frame, each site becomes a row, each species becomes a column
+list0 <- as.list(rep(0,ncol(arran.dat.pa))) # creates a list where each column name is assigned a value of 0
+names(list0) <- names(arran.dat.pa)
+arran.dat.pa <- as.data.frame(arran.dat.pa %>% replace_na(list0)) 
+# replace NA values with 0, representing species absence
+row.names(arran.dat.pa) <- arran.dat.pa$site #site columns are assigned as row names, columns are species
+arran.dat.pa <- arran.dat.pa[,-1] # delete the site column
+arran.dat.pa[arran.dat.pa > 0] <- 1 # converts count data to presence (1), absences are 0
+
+View(arran.dat.pa)
