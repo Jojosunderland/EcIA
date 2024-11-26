f32d6af721ad716eea2bead891e3533bd0f1c913
Jojosunderland <jojolucysunderland@gmail.com>
2024-11-26 23:15:53 +0000

    site by species presence/absence matrix made

--- Vert_tech.R
+++ Vert_tech.R
@@ -30,4 +30,27 @@ vert.tech.dat.south <-vert.tech.dat[which(vert.tech.dat$site=="South"),]
 length(unique(vert.tech.dat.north$scientificName)) # 6 unique species
 length(unique(vert.tech.dat.south$scientificName)) # 4 unique species
 
+## Create a site by species matrix ##
+
+# ONLY presence/absence data
+vert.tech.dat.new <- vert.tech.dat[,c("scientificName","site")] 
+vert.tech.dat.new$presence <- 1  # adds a new column presences (sets everything to 1s)
+
+# group by site, order and sum the values (presence data) for each group 
+vert.tech.dat.new <- vert.tech.dat.new %>%
+  group_by(site, scientificName) %>%
+  summarise(presence = sum(presence), .groups = "drop")
+
+vert.tech.dat.pa <- vert.tech.dat.new %>% 
+  pivot_wider(names_from=scientificName,values_from=c(presence)) 
+# pivot_wider creates a wide-format data frame, each site becomes a row, each species becomes a column
+list0 <- as.list(rep(0,ncol(vert.tech.dat.pa))) # creates a list where each column name is assigned a value of 0
+names(list0) <- names(vert.tech.dat.pa)
+vert.tech.dat.pa <- as.data.frame(vert.tech.dat.pa %>% replace_na(list0)) 
+# replace NA values with 0, representing species absence
+row.names(vert.tech.dat.pa) <- vert.tech.dat.pa$site #site columns are assigned as row names, columns are species
+vert.tech.dat.pa <- vert.tech.dat.pa[,-1] # delete the site column
+vert.tech.dat.pa[vert.tech.dat.pa > 0] <- 1 # converts count data to presence (1), absences are 0
+
+View(vert.tech.dat.pa)
 
