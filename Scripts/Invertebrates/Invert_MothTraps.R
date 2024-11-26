eee638eb61ceca34f0bb3f9a11b68fabe5ceced1
Jojosunderland <jojolucysunderland@gmail.com>
2024-11-26 20:57:08 +0000

    dataframes made for each site and species richness calculated

--- Scripts/Invertebrates/Invert_MothTraps.R
+++ Scripts/Invertebrates/Invert_MothTraps.R
@@ -14,4 +14,19 @@ library('dplyr')
 invert.moth <-
   read_xlsx("~/Documents/WorkingD/EcIA/Data/Arran_GBIF_data.xlsx",sheet="Moth traps")
 
-View(invert.field)
+View(invert.moth)
+
+# extract only the columns i'm interested in: site, individualCount and order
+invert.moth.dat <- select(invert.moth, site, order, individualCount)
+
+View(invert.moth.dat)
+# no NAs needed to be removed
+
+# create dataframes for each site (north = B, south = A) - keeping them named as north and south here to match the data. 
+invert.moth.dat.north <-invert.moth.dat[which(invert.moth.dat$site=="North"),]
+invert.moth.dat.south <-invert.moth.dat[which(invert.moth.dat$site=="South"),]
+
+# calculate how many unique orders are there between the sites
+length(unique(invert.moth.dat.north$order)) # 5 unique orders
+length(unique(invert.moth.dat.south$order)) # 3 unique orders
+
